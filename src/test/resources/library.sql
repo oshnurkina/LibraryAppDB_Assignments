c7237259535ed05f5f1fab8f224dd5a9723acc07
Oksana <oshnurkina@gmail.com>
2023-04-01 01:22:07 -0400

    BookPage_StepDefinition is complete

--- src/test/resources/library.sql
+++ src/test/resources/library.sql
@@ -1,10 +1,10 @@
 
 -- US 01
 select count(id) from users; --
--- 1855
+-- 1856
 
 select count(distinct id) from users;
--- 1855
+-- 1856
 
 
 -- RESULT --> MANUALLY IT IS PASSED
@@ -18,3 +18,10 @@ select * from users;
 -- US 03
 select count(*) from book_borrow
 where is_returned=0;
+
+select count(*) from books where name='Clean Code';
+
+select * from books where name='Clean Code';
+
+select b.name as bookName, author, bc.name as bookCategoryName from books b inner join book_categories
+    bc on b.book_category_id = bc.id where b.name = 'Lord of the Files';
